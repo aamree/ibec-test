0579c21074faef53d4baea3c761bb8683efc4281
aamree <adilet90@mail.ru>
2020-10-11 19:29:29 +0600

    init TopHeader

---
+++ src/components/topHeader/TopHeader.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface ITopHeaderProps {
+}
+
+export default function TopHeader (props: ITopHeaderProps) {
+  return (
+    <div className="container-fluid">
+      Жанна Арматурина
+    </div>
+  );
+}
